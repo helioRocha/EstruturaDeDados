b30b06d1e7b4f427ad580171e745af7395010a53
Helio Rocha <heliorocha@about.me>
2016-07-31 08:45:30 -0300

    Create No.java

---
+++ 04/PILHA-DINAMICA/No.java
@@ -0,0 +1,27 @@
+package br.edu.ifce.estruturadados.pilhadinamica;
+ 
+public class No<E> {
+     
+    private E elemento;
+    private No<E> proximo;
+     
+    public No(E elemento, No<E> proximo) {
+        this.elemento = elemento;
+        this.proximo = proximo;
+    }
+     
+    public E getElemento() {
+        return elemento;
+    }
+    public void setElemento(E elemento) {
+        this.elemento = elemento;
+    }
+    public No<E> getProximo() {
+        return proximo;
+    }
+    public void setProximo(No<E> proximo) {
+        this.proximo = proximo;
+    }
+     
+     
+}
