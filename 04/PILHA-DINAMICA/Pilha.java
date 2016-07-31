13b1a73be64c87d87fc5680f56acff1f731d00f0
Helio Rocha <heliorocha@about.me>
2016-07-31 08:46:06 -0300

    Create Pilha.java

---
+++ 04/PILHA-DINAMICA/Pilha.java
@@ -0,0 +1,15 @@
+package br.edu.ifce.estruturadados.pilhadinamica;
+ 
+public interface Pilha<E> {
+ 
+    /** Metodos Principais */
+     
+    public abstract No<E> insereTopo(E elemento);
+    public abstract No<E> removeTopo();
+     
+    /** Metodos Auxiliares */
+     
+    public abstract No<E> getTopo();
+    public abstract boolean pilhaVazia();
+    public abstract int tamanho();
+}
